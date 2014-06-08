f1c9727272abafe68868db07c224c88b85b949ff
loafer <zjh527@163.com>
2014-06-08 16:23:17 +0800

    add

---
+++ core-java-annotation/src/main/java/com/github/loafer/customannotations/ClassLeveAnnotation.java
@@ -0,0 +1,11 @@
+package com.github.loafer.customannotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Created by zhaojh on 14-6-5.
+ */
+@Target(ElementType.TYPE)
+public @interface ClassLeveAnnotation {
+}
